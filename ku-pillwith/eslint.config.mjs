7a37b9e3dae57c5aa63d7eeb2bc42efb5804af01
zziglet <jjw04033@konkuk.ac.kr>
2024-10-12 14:33:25 +0900

    style: Button 컴포넌트 추가 및 Eslint proptypes 설정 off (#3)
    버튼 컴포넌트 추가 및 eslint 설정 변경

--- ku-pillwith/eslint.config.mjs
+++ ku-pillwith/eslint.config.mjs
@@ -1,11 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-
+import { rules } from "@eslint/js/src/configs/eslint-all";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: { "react/prop-types": 0 },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
---
+++ ku-pillwith/src/components/Button.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+//전체 button style
+
+const StyledButton = styled.button`
+  width: 83px;
+  height: 32px;
+  display: inline-block;
+  text-align: center;
+  overflow: hidden;
+  padding: 8px;
+  font-family: "Inter-Regular", Helvetica;
+  font-weight: 400;
+  font-size: 16px;
+  color: white;
+  border-radius: 8px;
+
+  &.grey {
+    background-color: #808183;
+    border: 1px solid #808183;
+  }
+
+  &.green {
+    background-color: #8276f4;
+    border: 1px solid #8276f4;
+  }
+`;
+
+function Button(props) {
+  const { title, onClick, className } = props;
+  return (
+    <StyledButton onClick={onClick} className={className}>
+      {title || "button"}
+    </StyledButton>
+  );
+}
+
+export default Button;
