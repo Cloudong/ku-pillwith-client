9fe4046c464f361f5285caf13a704c35e623f9ae
zziglet <jjw04033@konkuk.ac.kr>
2024-10-19 20:32:13 +0900

    fix: data fetch 임시 기능 삭제
    서버의 스케줄링 도입 이슈

--- ku-pillwith/src/App.js
+++ ku-pillwith/src/App.js
@@ -1,31 +1,8 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import { UserProvider } from "./api/UserContext";
 import Router from "./route/Router";
 
 function App() {
-  const [pills, setPills] = useState([]);
-  //공공데이터 fetch
-  useEffect(() => {
-    const fetchData = async () => {
-      if (pills.length > 0) return;
-      try {
-        const response = await fetch("http://localhost:3001/pills/fetch");
-        if (response.ok) {
-          const data = await response.json();
-          setPills(data);
-          console.log("Data fetched and stored successfully");
-        } else {
-          console.error("Failed to fetch data");
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, [pills]);
-
   return (
     <UserProvider>
       <Router />
